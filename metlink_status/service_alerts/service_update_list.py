9134cdee7603865703e1fbf64435f9383597a339
Finn <81511282+finncodes@users.noreply.github.com>
2021-10-10 18:58:45 +1300

    PEP formatting fix

--- metlink_status/service_alerts/service_update_list.py
+++ metlink_status/service_alerts/service_update_list.py
@@ -12,7 +12,9 @@ class GTFSRTServiceUpdateList:
         response = requests.get("https://api.opendata.metlink.org.nz/v1/gtfs-rt/servicealerts",
                                 headers={
                                     'x-api-key': self.api_key,
-                                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.81',
+                                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ('
+                                                  'KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 '
+                                                  'Edg/88.0.705.81',
                                     'origin': 'https://www.metlink.org.nz',
                                     'referer': 'https://www.metlink.org.nz/alerts/train/today'
                                 }).json()
