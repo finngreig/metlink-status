0f173118fd0f396b519cf00ac29d1e7e585d1c34
Finn <81511282+finncodes@users.noreply.github.com>
2021-10-12 00:26:32 +1300

    Added rich for CLI tables

--- metlink_status/cli.py
+++ metlink_status/cli.py
@@ -3,6 +3,10 @@ from colorama import init as colorama_init
 from .helpers import APIHelper
 from .service_alerts import GTFSRTServiceUpdateList
 from .stop_departures import StopDepartureList
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
 
 
 def print_list(update_list, service=None):
@@ -13,8 +17,40 @@ def print_list(update_list, service=None):
         update_list (GTFSRTServiceUpdateList|StopDepartureList): The update list
         service (str): The user service to scope to
     """
-    for row in update_list.get_items(service):
-        print(row)
+    table = Table(show_header=True)
+
+    if isinstance(update_list, GTFSRTServiceUpdateList):
+        table.add_column("From", width=10)
+        table.add_column("Until", width=10)
+        table.add_column("Cause")
+        table.add_column("Effect")
+        table.add_column("Summary")
+        table.add_column("Description")
+        table.add_column("Severity")
+        table.add_column("Stops")
+        table.add_column("Routes")
+        # table.add_column("Link", width=50)
+
+        for row in update_list.get_items(service):
+            table.add_row(row.start_time, row.end_time, row.cause.value, row.effect.value, row.header, row.description,
+                          row.severity_level, ', '.join(row.stop_ids), ', '.join([str(rid) for rid in row.route_ids]))
+                          # row.url)
+    elif isinstance(update_list, StopDepartureList):
+        table.add_column("Time")
+        table.add_column("Delay")
+        table.add_column("Destination")
+        table.add_column("Origin")
+        table.add_column("Service")
+        table.add_column("Wheelchair Accessible")
+        table.add_column("Status")
+        table.add_column("Vehicle ID")
+
+        for row in update_list.get_items(service):
+            table.add_row(row.time.strftime("%m/%d/%Y, %H:%M:%S"), str(row.delay), f"{row.destination} - {row.destination_stop_id}",
+                          f"{row.origin} - {row.origin_stop_id}", str(row.service_id),
+                          f"{'Yes' if row.wheelchair_accessible else 'No'}", row.status, str(row.vehicle_id))
+
+    console.print(table)
 
 
 def main():
